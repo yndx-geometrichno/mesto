958b7f306d355b3a5bb9ac0e73d834d8da36227c
yndx_geometrichno <klementevart@yandex.ru>
2023-06-26 14:30:40 +0300

    feat: rendering precreated cards with JS

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ let popupSaveBtn = document.querySelector('.popup__save-btn');
 let popupForm = document.querySelector('.popup__form');
 let inputName = document.querySelector('.popup__input_type_name');
 let inputJob = document.querySelector('.popup__input_type_job');
+let cardsContainer = document.querySelector('.cards');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 inputName.value = profileName.textContent;
 inputJob.value = profileJob.textContent;
@@ -28,3 +56,24 @@ function handleFormSubmit (evt) {
 }
 
 popupForm.addEventListener('submit', handleFormSubmit);
+
+
+// Достаем из документа заготовку для карточки с фото
+let templateCard = document.querySelector('.template-card');
+
+// Функция, добавляющая новую карточку
+const addCard = ({name, link}) => {
+  let clone = templateCard.content.cloneNode(true);
+  let cardElem = clone.querySelector('.card');
+  cardElem.querySelector('.card__img').src = link;
+  cardElem.querySelector('.card__img').alt = name;
+  cardElem.querySelector('.card__name').textContent = name;
+  return cardElem;
+};
+
+
+// Добавляем на страницу заготовки карточек
+initialCards.forEach((item) => {
+  const cardElement = addCard(item);
+  cardsContainer.append(cardElement);
+});
